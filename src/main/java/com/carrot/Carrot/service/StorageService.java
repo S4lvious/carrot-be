de6e2a3e3d18d41841e74a9e6aec3bd9aca7e5f7
Salvatore Liccardo <s.liccardo022@gmail.com>
2025-02-21 16:18:41 +0100

    fix gcloud credentials

--- src/main/java/com/carrot/Carrot/service/StorageService.java
+++ src/main/java/com/carrot/Carrot/service/StorageService.java
@@ -1,9 +1,12 @@
 package com.carrot.Carrot.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -14,12 +17,17 @@ public class StorageService {
     private final Storage storage;
     private final String bucketName;
 
-    public StorageService(@Value("${spring.cloud.gcp.storage.bucket}") String bucketName) {
-        this.storage = StorageOptions.getDefaultInstance().getService();
+    public StorageService(@Value("${spring.cloud.gcp.credentials.location}") String credentialsPath,
+                          @Value("${spring.cloud.gcp.storage.bucket}") String bucketName) throws IOException {
         this.bucketName = bucketName;
+
+        // ✅ Carica manualmente le credenziali da /etc/secrets/google-credentials.json
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath))
+                .createScoped("https://www.googleapis.com/auth/cloud-platform");
+        this.storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
     }
 
-    // 📌 Metodo per caricare un file
+    // 📌 Metodo per caricare un file su GCS
     public String uploadFile(MultipartFile file, String ordineId) throws IOException {
         String fileName = "ordini/" + ordineId + "/" + file.getOriginalFilename();
 
